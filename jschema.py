011b9fb466a57770f7f664ca32fb12407ef00994
Rob Earwaker <rob.earwaker@gmail.com>
2016-10-28 20:15:37 +0100

    Removed old code

--- jschema.py
+++ jschema.py
@@ -385,35 +385,13 @@ class JSchema(object):
         unique_items = kwargs.get(UNIQUE_ITEMS_KEY, None)
         validate_unique_items(unique_items)
 
-        self._optional = kwargs.pop('optional', False)
-        schema = {}
+        self._dict = {}
         for field, field_name in self.FIELD_NAMES.items():
             if field in kwargs:
-                schema[field_name] = kwargs[field]
-        if 'ref' in kwargs:
-            self._dict = {
-                'definitions': schema.pop('definitions', {}),
-                '$ref': '#/definitions/{0}'.format(kwargs['ref'])
-            }
-            self._dict['definitions'][kwargs['ref']] = schema
-        else:
-            self._dict = schema
-
-    @property
-    def optional(self):
-        return self._optional
-
-    @property
-    def definitions(self):
-        return self._dict.get('definitions', {})
+                self._dict[field_name] = kwargs[field]
 
     def asdict(self, root=True, id=None):
-        dict = self._dict.copy()
-        if not root:
-            dict.pop('definitions', None)
-        if id:
-            dict['id'] = id
-        return dict
+        return self._dict.copy()
 
     def asjson(self, pretty=False):
         indent = 4 if pretty else None
@@ -427,143 +405,7 @@ def uname():
     return uuid.uuid4().hex
 
 
-def Properties(**kwargs):
-    return kwargs
-
-
-def Dependencies(**kwargs):
-    return kwargs
-
-
 class JSchemaMeta(type):
     def __call__(cls, *args, **kwargs):
         jschema = super(JSchemaMeta, cls).__call__(*args, **kwargs)
         return type(uname(), (object,), {'jschema': jschema})
-
-
-class Array(JSchema, metaclass=JSchemaMeta):
-    def __init__(self, **kwargs):
-        definitions = {}
-        additional_items = kwargs.get('additional_items', None)
-        if hasattr(additional_items, 'jschema'):
-            schema = additional_items.jschema
-            kwargs['additional_items'] = schema.asdict(root=False)
-            for name in schema.definitions:
-                definitions[name] = schema.definitions[name]
-        items = kwargs.get('items', None)
-        if isinstance(items, list):
-            kwargs['items'] = []
-            for item in items:
-                schema = item.jschema
-                kwargs['items'].append(schema.asdict(root=False))
-                for name in schema.definitions:
-                    definitions[name] = schema.definitions[name]
-        elif hasattr(items, 'jschema'):
-            schema = items.jschema
-            kwargs['items'] = schema.asdict(root=False)
-            for name in schema.definitions:
-                definitions[name] = schema.definitions[name]
-        if definitions:
-            kwargs['definitions'] = definitions
-        kwargs['type'] = 'array'
-        super(Array, self).__init__(**kwargs)
-
-
-class Boolean(JSchema, metaclass=JSchemaMeta):
-    def __init__(self, **kwargs):
-        kwargs['type'] = 'boolean'
-        super(Boolean, self).__init__(**kwargs)
-
-
-class Integer(JSchema, metaclass=JSchemaMeta):
-    def __init__(self, **kwargs):
-        kwargs['type'] = 'integer'
-        super(Integer, self).__init__(**kwargs)
-
-
-class Null(JSchema, metaclass=JSchemaMeta):
-    def __init__(self, **kwargs):
-        kwargs['type'] = 'null'
-        super(Null, self).__init__(**kwargs)
-
-
-class Number(JSchema, metaclass=JSchemaMeta):
-    def __init__(self, **kwargs):
-        kwargs['type'] = 'number'
-        super(Number, self).__init__(**kwargs)
-
-
-class Object(JSchema, metaclass=JSchemaMeta):
-    def __init__(self, **kwargs):
-        definitions = {}
-        additional_properties = kwargs.get('additional_properties', None)
-        if hasattr(additional_properties, 'jschema'):
-            schema = additional_properties.jschema
-            kwargs['additional_properties'] = schema.asdict(root=False)
-            for name in schema.definitions:
-                definitions[name] = schema.definitions[name]
-        properties = kwargs.get('properties', {})
-        for name in sorted(properties):
-            schema = properties[name].jschema
-            kwargs['properties'][name] = schema.asdict(root=False)
-            if not schema.optional:
-                if 'required' not in kwargs:
-                    kwargs['required'] = []
-                kwargs['required'].append(name)
-            for name in schema.definitions:
-                definitions[name] = schema.definitions[name]
-        pattern_properties = kwargs.get('pattern_properties', {})
-        for name in pattern_properties:
-            schema = pattern_properties[name].jschema
-            kwargs['pattern_properties'][name] = schema.asdict(root=False)
-            for name in schema.definitions:
-                definitions[name] = schema.definitions[name]
-        dependencies = kwargs.get('dependencies', {})
-        for name, dependency in dependencies.items():
-            if hasattr(dependency, 'jschema'):
-                schema = dependency.jschema
-                kwargs['dependencies'][name] = schema.asdict(root=False)
-                for name in schema.definitions:
-                    definitions[name] = schema.definitions[name]
-        if definitions:
-            kwargs['definitions'] = definitions
-        kwargs['type'] = 'object'
-        super(Object, self).__init__(**kwargs)
-
-
-class String(JSchema, metaclass=JSchemaMeta):
-    def __init__(self, **kwargs):
-        kwargs['type'] = 'string'
-        super(String, self).__init__(**kwargs)
-
-
-class Empty(JSchema, metaclass=JSchemaMeta):
-    def __init__(self, **kwargs):
-        definitions = {}
-        all_of = kwargs.pop('all_of', [])
-        for type in all_of:
-            schema = type.jschema
-            if 'all_of' not in kwargs:
-                kwargs['all_of'] = []
-            kwargs['all_of'].append(schema.asdict(root=False))
-            for name in schema.definitions:
-                definitions[name] = schema.definitions[name]
-        any_of = kwargs.pop('any_of', [])
-        for type in any_of:
-            schema = type.jschema
-            if 'any_of' not in kwargs:
-                kwargs['any_of'] = []
-            kwargs['any_of'].append(schema.asdict(root=False))
-            for name in schema.definitions:
-                definitions[name] = schema.definitions[name]
-        one_of = kwargs.pop('one_of', [])
-        for type in one_of:
-            schema = type.jschema
-            if 'one_of' not in kwargs:
-                kwargs['one_of'] = []
-            kwargs['one_of'].append(schema.asdict(root=False))
-            for name in schema.definitions:
-                definitions[name] = schema.definitions[name]
-        if definitions:
-            kwargs['definitions'] = definitions
-        super(Empty, self).__init__(**kwargs)
