a1a9036726ee5c1c01da981d269031e2bd79fd27
Rob Earwaker <rob.earwaker@gmail.com>
2016-11-01 19:46:58 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='jsch',
+    version='0.1.0',
+    description=(
+        'Define classes that conform to a JSON schema, with built-in'
+        'validation and schema generation.'
+    ),
+    url='https://github.com/rob-earwaker/jsch',
+    author='Rob Earwaker',
+    license='MIT',
+    packages=['jsch']
+)
