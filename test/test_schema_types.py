e1b39ca39cf2a35e523763f74755b35363a4c647
Rob Earwaker <rob.earwaker@gmail.com>
2016-11-03 21:22:37 +0000

    Changed schema type test organisation

--- test/test_schema_types.py
+++ test/test_schema_types.py
@@ -3,43 +3,31 @@ import unittest
 import jsch
 
 
-class TestArrayType(unittest.TestCase):
-    def test_type_keyword(self):
+class TestSchemaTypes(unittest.TestCase):
+    def test_array_type_keyword(self):
         schema = jsch.Array()
         self.assertEqual('array', schema.type)
 
-
-class TestBooleanType(unittest.TestCase):
-    def test_type_keyword(self):
+    def test_boolean_type_keyword(self):
         schema = jsch.Boolean()
         self.assertEqual('boolean', schema.type)
 
-
-class TestIntegerType(unittest.TestCase):
-    def test_type_keyword(self):
+    def test_integer_type_keyword(self):
         schema = jsch.Integer()
         self.assertEqual('integer', schema.type)
 
-
-class TestNullType(unittest.TestCase):
-    def test_type_keyword(self):
+    def test_null_type_keyword(self):
         schema = jsch.Null()
         self.assertEqual('null', schema.type)
 
-
-class TestNumberType(unittest.TestCase):
-    def test_type_keyword(self):
+    def test_number_type_keyword(self):
         schema = jsch.Number()
         self.assertEqual('number', schema.type)
 
-
-class TestObjectType(unittest.TestCase):
-    def test_type_keyword(self):
+    def test_object_type_keyword(self):
         schema = jsch.Object()
         self.assertEqual('object', schema.type)
 
-
-class TestStringType(unittest.TestCase):
-    def test_type_keyword(self):
+    def test_string_type_keyword(self):
         schema = jsch.String()
         self.assertEqual('string', schema.type)
